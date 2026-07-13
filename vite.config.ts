a65f191fed2efc027f2939d1916c8431746ff287
sandy-25 <prajapatisandip625@gmail.com>
2026-07-13 16:41:29 +0530

    again changing base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/sandip-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
