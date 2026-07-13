8c2d0f093d24f51d7188844e886253e19a299589
sandy-25 <prajapatisandip625@gmail.com>
2026-07-13 16:17:59 +0530

    reset base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/sandip-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
